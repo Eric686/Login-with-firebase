1c8297bff1b8b4fe26d71a6d2b117b20a0383e27
EricSmart <61547098+Eric686@users.noreply.github.com>
2024-03-19 20:53:37 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+require("./add")
+
+console.log("Hello world");
+
+hello = () => "Good morning";
+console.log(hello());
+
+/*<p id="demo"></p>
+
+let hello = "";
+hello = val => "Hello" + val;
+document.getElementById("demo").innerHTML = hello("Baby Kendra");*/
